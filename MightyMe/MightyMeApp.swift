358cbb4eeadf76c82b2edad4ab047841a2dcbcf4
Boss Office <dfeetnsb@gmail.com>
2020-12-19 23:21:31 +0700

    Initial Commit

---
+++ MightyMe/MightyMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MightyMeApp.swift
+//  MightyMe
+//
+//  Created by GA006973 on 12/19/20.
+//
+
+import SwiftUI
+
+@main
+struct MightyMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
